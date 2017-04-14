315571bd517c24a64ca34d5210a9c962780b99fb
Håkon S. Møller <haakomol@ifi.uio.no>
2017-04-14 21:55:55 +0200

    More work on TruthTabler

--- truth-tabler.elm
+++ truth-tabler.elm
@@ -1,18 +1,84 @@
 module TruthTabler exposing (..)
 
+import Array exposing (Array, get)
+
 import Types exposing (..)
 
 type alias Propvar = String
 
-type ValuationTree
-  = Leaf Bool Propvar
-  | Not Bool ValuationTree
-  | Binary Bool ValuationTree BinaryOperator ValuationTree
+type SemanticTree
+  = SemLeaf Bool Propvar
+  | SemNot Bool SemanticTree
+  | SemBinary Bool SemanticTree BinaryOperator SemanticTree
 
-growValuationTree : ParseTree -> List Bool -> List Propvar -> ValuationTree
-growValuationTree parseTree truthValues propvars =
-  
-  
+semTreeValue : SemanticTree -> Bool
+semTreeValue semanticTree =
+  case semanticTree of
+    SemLeaf value _ ->
+      value
+    SemNot value _ ->
+      value
+    SemBinary value _ _ _ ->
+      value
+
+growValuationTree : ParseTree -> (Array Propvar) -> (Array Bool) -> SemanticTree
+growValuationTree parseTree propvars truthValues =
+  case parseTree of
+    Leaf propvar ->
+      SemLeaf (findValForPropvar propvar propvars truthValues) propvar
+    Not subTree ->
+      let
+        semSubTree = growValuationTree subTree propvars truthValues
+        subTreeValue = semTreeValue semSubTree
+      in
+        SemNot (not subTreeValue) semSubTree
+    Binary left op right ->
+      let
+        semLeftTree = growValuationTree left propvars truthValues
+        semRightTree = growValuationTree right propvars truthValues
+        leftValue = semTreeValue semLeftTree
+        rightValue = semTreeValue semRightTree
+        combinedValue = binaryValue op leftValue rightValue
+      in
+        SemBinary combinedValue semLeftTree op semRightTree
+
+findValForPropvar : Propvar -> Array Propvar -> Array Bool -> Bool
+findValForPropvar propvar propvars truthValues =
+  let
+    propVarIndex = indexInArray propvar propvars
+    truthValueMaybe = Array.get propVarIndex truthValues
+  in
+    case truthValueMaybe of
+      Nothing -> False
+      Just truthValue -> truthValue
+
+indexInArray : a -> Array a -> Int
+indexInArray elToLookFor array =
+  let
+    innerHelper index =
+      let
+        elAtIndexMaybe = Array.get index array
+      in
+        case elAtIndexMaybe of
+          Nothing -> -1
+          Just elAtIndex ->
+            if   elAtIndex == elToLookFor
+            then index
+            else innerHelper (index + 1)
+  in
+    innerHelper 0
+
+binaryValue : BinaryOperator -> Bool -> Bool -> Bool
+binaryValue op val1 val2 =
+  case op of
+    And ->
+      val1 && val2
+    Or ->
+      val1 || val2
+    Implication ->
+      (not val1) || val2
+
+
 collectPropvars : ParseTree -> List Propvar
 collectPropvars parseTree =
   case parseTree of
@@ -31,14 +97,14 @@ collectPropvars parseTree =
 
 getUniquePropvars : List Propvar -> List Propvar
 getUniquePropvars propvars =
-  reduceToUniques propvars
+  removeDuplicates propvars
 
-reduceToUniques : List a -> List a
-reduceToUniques lst =
+removeDuplicates : List a -> List a
+removeDuplicates lst =
   case lst of
     [] -> []
     x :: rest ->
-      x :: (reduceToUniques (filterFromList x rest))
+      x :: (removeDuplicates (filterFromList x rest))
 
 filterFromList : a -> List a -> List a
 filterFromList el lst =
